f487c9b887e96c3371942c82266dc2773824db00
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2020-12-11 18:50:21 -0300

    Refatorando o projeto por problema em imcompatibilidade de versão

--- database/database.js
+++ database/database.js
@@ -1,7 +1,6 @@
 const Sequelize = require("sequelize");
 
-
-const connection = new Sequelize('guiapress', 'root', '',{
+const connection = new Sequelize('guiapress','root','',{
     host: 'localhost',
     dialect: 'mysql'
 });
--- index.js
+++ index.js
@@ -1,48 +1,25 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const connection = require("./database/database");
-
-
-////////// IMPORTANDO AS ROTAS \\\\\\\\\\
-const categoriesController = require("./controller/CategoriesController");
-const articlesController = require("./controller/ArticlesController");
-
-
-////////// IMPORTANDO OS MODEL \\\\\\\\\\
-const Article = require("./model/Article");
-const Category = require("./model/Category");
+const connection = require("./.git")
 
 ////////// CONFIGURANDO VIEW ENGINE \\\\\\\\\\
 app.set('view engine', 'ejs');
 
-////////// CONFIGURANDO ARQUIVOS STATIC \\\\\\\\\\
+////////// CONFIGURANDO DE ARQUIVOS STATIC \\\\\\\\\\
 app.use(express.static('public'));
- 
-////////// CONFIGURANDO BODY PARSER \\\\\\\\\\
+
+////////// CONFIGURANDO BODY PARSER\\\\\\\\\\
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-////////// CONFIGURANDO CONEXAO COM BANCO DE DADOS \\\\\\\\\\
-connection.authenticate()
-    .then(() => {
-        console.log("Conexão feita com sucesso!");
-    }).catch((error) => {
-        console.log(error);
-    });
-
-////////// DIFININDO USO DAS ROTAS \\\\\\\\\\
-app.use("/", categoriesController);
-app.use("/", articlesController);
-
-////////// MINHA ROTAS \\\\\\\\\\
-app.get("/", (req, res) => {
-    res.render("index");
+app.get('/', (req, res) => {
+    res.render("index")
 });
 
 
 
-/////////// INICIALIZANDO MEU SERVIDOR \\\\\\\\\\
+////////// INICIALIZANDO SERVIDOR \\\\\\\\\\
 app.listen(3000, () => {
     console.log("SERVIDOR ONLINE");
-});
+}); 

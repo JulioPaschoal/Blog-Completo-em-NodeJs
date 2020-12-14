14973532b841be2061ae3febd009a5824903c45b
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2020-12-14 15:41:01 -0300

    Criando tela de listar categorias

--- controller/CategoriesController.js
+++ controller/CategoriesController.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const Category = require("../model/Category");
 const slugify = require("slugify");
 
+
+////////// ROTA DO FORMULARIO DE CADASTRO \\\\\\\\\\
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new")
 });
 
+
+////////// SALVANDO DADOS NO BANCO \\\\\\\\\\
 router.post("/categories/save", (req, res) => {
     var title = req.body.title;
     if (title != undefined) {
@@ -21,6 +25,15 @@ router.post("/categories/save", (req, res) => {
     }
 });
 
+
+////////// LISTANDO DADOS DE CATEGORIA \\\\\\\\\\
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {
+            categories: categories
+        });
+    });
+});
 
 
 module.exports = router;

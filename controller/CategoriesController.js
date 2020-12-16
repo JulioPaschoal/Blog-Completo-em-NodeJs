0f094c61a1d24e0a00abcba5769740a439a6a37b
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2020-12-16 18:50:48 -0300

    Criando formulario de edição e deletando categoria

--- controller/CategoriesController.js
+++ controller/CategoriesController.js
@@ -18,7 +18,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect("/")
+            res.redirect("/admin/categories")
         })
     } else {
         res.redirect("/admin/categories/new");
@@ -35,5 +35,41 @@ router.get("/admin/categories", (req, res) => {
     });
 });
 
+////////// DELETAR CATEGORIA DO BANCO \\\\\\\\\\
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+        } else { ////////// SE O ID NÃO FOR UM NUMERO \\\\\\\\\\
+            res.redirect("/admin/categories");
+        }
+    } else {////////// SE O ID FOR NULO \\\\\\\\\\
+        res.redirect("/admin/categories");
+    }
+});
+
+////////// EDITAR UMA CATEGORIA \\\\\\\\\\
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", { category: category })
+        } else {
+            res.redirect("/admin/categories");
+        }
+    }).catch(error => {
+        res.redirect("/admin/categories");
+    });
+})
 
 module.exports = router;

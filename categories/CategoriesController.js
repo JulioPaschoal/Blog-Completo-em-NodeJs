395410c1f0ef462de1b7371e047b42f486a11846
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2021-05-17 22:09:10 -0300

    Cadastrando uma nova categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
 
-// ROTA DA CATEGORIA \\
-router.get("/categories", (req, res) => {
-    res.send("Rota de Categoria!");
+
+
+// ROTA PARA LISTAR AS CATEGORIAS \\
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(Category => {
+        res.render("admin/categories/index", { Category: Category });
+    });
 });
 
-// ROTA PARA CRIARA UMA NOVA CATEGORIA \\
+// ROTA PARA CRIAR UMA NOVA CATEGORIA \\
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
 });
+
+// ROTA PARA SALVAR CATEGORIA NO BANDO DE DADOS \\
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if (title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/");
+        });
+    } else {
+        res.redirect("/admin/categories/new");
+    }
+});
+
+
 
 
 module.exports = router;
